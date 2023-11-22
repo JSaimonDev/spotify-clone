b8819b18df80ba669cb6c60427ee7e37350c3f54
Disco <jjotasaimon@gmail.com>
2023-11-22 13:30:14 +0700

    Recovering state previoius crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), react(), tailwind()]
+});
